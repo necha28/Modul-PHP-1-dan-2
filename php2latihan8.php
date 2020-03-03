a513df76b7690edfffb015a213b4cd722c4adb84
Your Name <you@example.com>
2020-03-03 12:28:05 +0700

    first commit

---
+++ php2latihan8.php
@@ -0,0 +1,14 @@
+<?php
+
+$arrNilai = array ("Ani" => 80, "otim" => 90, "Ana" => 75, "Budi" => 85);
+echo "Ani= " .$arrNilai['Ani'];         //80
+echo "Otim= ".$arrNilai['otim'];        //90
+
+$arrNilai = array();
+$arrNilai ['Ami'] = 80;
+$arrNilai ['Asma'] = 95;
+$arrNilai ['Sri'] = 77;
+
+echo "Asma= " .$arrNilai ['Asma'];       //95
+echo "Ami= " .$arrNilai ['Ami'];         //80
+?>
